f6620e209b6ba76201efbab53dcd56dda3e69eb8
Julien GOTTELAND <julien.gotteland@gmail.com>
2023-01-07 12:38:40 +0100

    Add day template

---
+++ template/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "aoc2021/utils"
+
+func parse(fileName string) any {
+	return nil
+}
+
+func part1(input any) {
+	result := 0
+
+	utils.Check(1, 0, result)
+}
+
+func part2(input any) {
+	result := 0
+
+	utils.Check(2, 0, result)
+}
+
+func main() {
+	input := parse("input.txt")
+
+	part1(input)
+	part2(input)
+}
